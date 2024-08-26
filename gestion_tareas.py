b5a7a3f47aead87a84a4afd1842b92b5fbd451b6
willian <willianangulo315@gmail.com>
2024-08-25 21:23:49 -0500

    Añadir Gestiones Basicas de Gestion de Tareas

---
+++ gestion_tareas.py
@@ -0,0 +1,47 @@
+tareas = []
+
+def mostrar_tareas():
+    if not tareas:
+        print("No hay tareas pendientes")
+    else:
+        print("Tareas pendientes:")
+        for i, tarea in enumerate(tareas, start=1):
+            print(f"{i}. {tarea}")
+            
+def agregar_tarea(tarea):
+    tareas.append(tarea)
+    print(f"La tarea '{tarea}' ha sido agregada")
+    
+def eliminar_tarea(indice):
+    try:
+        tareas.pop(indice - 1)
+        print("La tarea ha sido eliminada")
+    except IndexError:
+        print("El índice de la tarea no existe")
+        
+def menu():
+    print("\n Gestion de Tareas")
+    print("1. Mostrar tareas")
+    print("2. Agregar tarea")
+    print("3. Eliminar tarea")
+    print("4. Salir")
+    opcion = input("Ingresa una opción: ")
+    
+if __name__ == "__main__":
+    while True:
+        menu()
+        
+        option = input("Ingresa una opción: ")
+        if option == "1":
+            mostrar_tareas()
+        elif option == "2":
+            tarea = input("Ingresa la nueva tarea: ")
+            agregar_tarea(tarea)
+        elif option == "3":
+            indice = int(input("Ingresa el índice de la tarea a eliminar: "))
+            eliminar_tarea(indice)
+        elif option == "4":
+            print("Hasta luego")
+            break
+        else:
+            print("Opción inválida. Por favor, elige una opción válida.")
